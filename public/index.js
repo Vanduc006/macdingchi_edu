2a5d2c79651f9cd6c35caa4a8fb3182af3d7fd05
Vanduc006 <bagiadaohoa@gmail.com>
2023-10-23 16:12:08 +0700

    xong card ui

---
+++ public/index.js
@@ -0,0 +1,18 @@
+// var currentURL = window.location.href;
+// // Sử dụng URLSearchParams để trích xuất tham số từ URL
+// var urlParams = new URLSearchParams(currentURL);
+// // Lấy giá trị của tham số 'id' từ URL
+// var id = urlParams.get('id');
+// console.log(id)
+
+const titleVideo = document.querySelector('.title-video');
+const titlePopup = document.querySelector('.title-popup');
+
+titleVideo.addEventListener('mouseover', function () {
+    titlePopup.style.display = 'block';
+});
+  
+titleVideo.addEventListener('mouseout', function () {
+    titlePopup.style.display = 'none';
+});
+  
